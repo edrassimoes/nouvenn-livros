c5a6d6d4b2686a2da4239fc6823b05ee193cfedd
edrassimoes <edrassimoes@gmail.com>
2024-02-16 00:28:24 -0300

    adicionando docker-compose

--- sql/init.sql
+++ sql/init.sql
@@ -1,9 +1,4 @@
 CREATE DATABASE postgres;
-CREATE USER postgres WITH ENCRYPTED PASSWORD 'postgres';
-ALTER ROLE postgres SET client_encoding TO 'utf8';
-ALTER ROLE postgres SET default_transaction_isolation TO 'read committed';
-ALTER ROLE postgres SET timezone TO 'UTC';
-GRANT ALL PRIVILEGES ON DATABASE postgres TO postgres;
 
 create table if not exists users
 (
